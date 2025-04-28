eb4d2f667a4dea2d7008a879f459b011afbbc40f
fitsum <fitbez@gmail.com>
2025-04-28 18:59:20 -0400

    used dotenv to access db infromation

--- database.js
+++ database.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config(); //enable dotenv
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("employee_app", "root", "P@ssword1", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
 try {
   sequelize.authenticate();
